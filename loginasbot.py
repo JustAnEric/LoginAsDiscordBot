11c9ade899136b9f73348ea61351a3e068aeba43
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 18:01:38 +1100

    Create loginasbot.py

---
+++ loginasbot.py
@@ -0,0 +1,9 @@
+import tkinter, pywebview, os, re, json, time, main, subprocess
+from urllib.request import Request, urlopen
+
+print("Starting...")
+i = input("What is your bot token? Please paste it in: ")
+
+window = tkinter.Tk()
+
+window.mainloop()
