4c76fc76890a33c068412b6be1d03a6471700261
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 18:18:39 +1100

    Update loginasbot.py

--- loginasbot.py
+++ loginasbot.py
@@ -3,6 +3,10 @@ from urllib.request import Request, urlopen
 
 print("Starting...")
 i = input("What is your bot token? Please paste it in: ")
+try:
+  print(main.getBotByToken(i))
+  print(f"Logging in as {main.getBotByToken(i)['name']}...")
+except: print("We encountered an error during the account information retrieval process."); exit();
 
 window = tkinter.Tk()
 
