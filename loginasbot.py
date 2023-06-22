80817205485afd7aadee68db52ca3d63bbb9fe8e
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-06-23 09:59:32 +1000

    cool

--- loginasbot.py
+++ loginasbot.py
@@ -1,13 +1,27 @@
-import tkinter, pywebview, os, re, json, time, main, subprocess
+import tkinter, os, re, json, time, subprocess, flask
+from json import loads, dumps
 from urllib.request import Request, urlopen
+
+def getBotByToken(token=None):
+  if token:
+    return loads(urlopen(Request("https://discord.com/api/v6/users/@me", headers={"Bearer Authorization": token})).read().decode())
+  else: return print("Please specify a bot token that the program can get information from.")
+
 
 print("Starting...")
 i = input("What is your bot token? Please paste it in: ")
 try:
-  print(main.getBotByToken(i))
-  print(f"Logging in as {main.getBotByToken(i)['name']}...")
+  print(getBotByToken(i))
+  print(f"Logging in as {getBotByToken(i)['name']}...")
 except: print("We encountered an error during the account information retrieval process."); exit();
 
-window = tkinter.Tk()
+load = flask.Flask('server')
+@load.route('/')
+def home():
+  return flask.redirect("/login")
 
-window.mainloop()
+@load.route('/login')
+def login_as_discord_bot():
+  return flask.render_template('login.html')
+
+load.run(port=8080)
